0707d2e0884d6b73049dbb37480b7fb5d089cb24
erdal <erdal.ustalar@gmail.com>
2022-10-10 13:11:32 +0200

    Missat export  default på herosection fixat nu

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,30 +1,33 @@
-import React from 'react'
+import React from 'react';
 import '../App.css';
-import {Button} from './Button';
+import { Button } from './Button';
 import './HeroSection.css';
 
 function HeroSection() {
   return (
-    <div className='hero-container' style={{aligen: "center"}} >
-        <video src='/videos/video-2.mp4' autoplay loop muted></video>
-        <h1>NU BÖRJAR ÄVENTYRET</h1>
-        <p> Vad väntar du på lets goo</p>
-        <div className='hero-btns'>
-            <Button 
-             className="btns"
-             buttonStyle="btn--outline" 
-             buttonSize=" btn--large"> 
-            LETS GOO
-            </Button>
-            <Button
-                className='btns'
-                buttonStyle='btn--primaary'
-                buttonSize='btn--large'>
-                    titta på trailer <i className='far fa-play-circle'/>
-            </Button>
-        </div>
+    <div className='hero-container'>
+      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <h1>ADVENTURE AWAITS</h1>
+      <p>What are you waiting for?</p>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
+        >
+          WATCH TRAILER <i className='far fa-play-circle' />
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
