1ccadd8c9611f5b82e432e9d6c71c094b397b088
erdal <erdal.ustalar@gmail.com>
2022-10-10 13:24:34 +0200

    fuska lite så det gick lite snabbare med css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
      <Routes>
        
        <Route exact path="/" element={<Home/>}/>
+       <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
        </Routes>
      
      </Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,54 +1,80 @@
-import React , {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
-import '../components/Navbar.css'
+import React, { useState, useEffect } from 'react';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
-    const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-    const hadnleClick = () => setClick(!click);
-    const closeMobilMenu = () => setClick(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-    const showButton = () => {
-        if(window.innerWidth <= 960 ) {
-            setButton(false)
-        } else {
-            setButton(true);
-        }
-    };
-    // för att ta bort sign in vid mobilläge i refresh
-    useEffect(()=> {
-        showButton();
-    }, []); 
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
 
-    window.addEventListener('resize', showButton);
-    return (
-        <nav className="navbar">
-            <div className="navbar-container">
-        <Link to="/" className="navbar-logo" onClick={closeMobilMenu}>
-         AhihiBeauty  <i className="fab fa-typo3"></i>
-        </Link>
-        <div className='menu-icon' onClick={hadnleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            TRVL
+            <i class='fab fa-typo3' />
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/services'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Services
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/products'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Products
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to='/sign-up'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
         </div>
-        <ul className={click ? 'nav-menu active' : 'nav-menu' }>
-            <li className='nav-item'>
-                <Link to='/' className='nav-links' onClick={closeMobilMenu}>Home</Link>
-            </li>
-            <li className='nav-item'>
-                <Link to='/Service' className='nav-links' onClick={closeMobilMenu}>Service</Link>
-            </li>
-            <li className='nav-item'>
-                <Link to='/product' className='nav-links' onClick={closeMobilMenu}>Products</Link>
-            </li>
-            <li className='nav-item'>
-                <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobilMenu}>Sign Up</Link>
-            </li>
-        </ul>
-        {button && <button buttonStyle='btn--outline'> SIGN UP</button>}
-     </div>
-    </nav>
-    )
+      </nav>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
