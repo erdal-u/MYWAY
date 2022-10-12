93d514b206f1a20389f151047a5d1d2d8b7727ef
erdal <erdal.ustalar@gmail.com>
2022-10-12 13:54:01 +0200

    test med cards i products

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import '../../App.css';
+import Cards from '../Cards';
 
 export default function Products() {
+  return(
+     <h1 className='products'>PRODUCTS
+  
+    <Cards />
+    
+    </h1>
+)
   return <h1 className='products'>PRODUCTS</h1>;
 }
