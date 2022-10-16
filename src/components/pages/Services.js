b25b8c33c1fe279456f24ba7ed5bbe59813b03a4
erdal <erdal.ustalar@gmail.com>
2022-10-16 13:54:27 +0200

    Försökte inlogg och fixa bilder

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import '../../App.css';
+import React from "react";
+import "../../App.css";
 
 export default function Services() {
- 
-  return <h1 className='services'><div >
-  <h1>Psoriasis food anti inflamatoriskt</h1>
-  </div>
+  return (
+    <h1 className="services">
+      <div>
+        <h1>Psoriasis food anti inflamatoriskt</h1>
+      </div>
 
-  <p>1. Bär t ex Jordgubbe Blåbär hallon mm</p>
-  <p>2. Spenat grönkål 
-ruccola  </p>
-<p> 3. Lax makril.</p>
-<p>Olive olja kokos olja.</p>
-  </h1>;
+      <p>1. Bär t ex Jordgubbe Blåbär hallon mm</p>
+      <p>2. Spenat grönkål ruccola </p>
+      <p> 3. Lax makril.</p>
+      <p>Olive olja kokos olja.</p>
+    </h1>
+  );
 }
